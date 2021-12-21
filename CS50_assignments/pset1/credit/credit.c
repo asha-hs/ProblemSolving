43e407d007b141e3027dba7c510a5d952a2e28e4
Asha Hakari <asha.hakari@gmail.com>
2021-12-20 22:43:51 -0800

    Update credit.c
    Complete implementation of credit.c

--- CS50_assignments/pset1/credit/credit.c
+++ CS50_assignments/pset1/credit/credit.c
@@ -10,20 +10,29 @@ int main(void)
     int digits_not_multiplied_sum = 0;
     int digit;
     int checksum = 0;
+    int seconddigit = 0;
 
-    while(cardnumber > 0)
+    while (cardnumber > 0)
     {
         digit = cardnumber % 10;
         count++;
         cardnumber = cardnumber / 10;
 
-        if(count % 2 == 0)
+
+        if (cardnumber < 10 && cardnumber > 0)
+        {
+
+            seconddigit = digit;
+
+        }
+
+        if (count % 2 == 0)
         {
             int copydigit = digit;
-            copydigit = copydigit *2;
+            copydigit = copydigit * 2;
 
             products_digits_sum = 0;
-            while(copydigit > 0)
+            while (copydigit > 0)
             {
                 products_digits_sum += copydigit % 10;
 
@@ -39,18 +48,20 @@ int main(void)
 
     }
 
+
     checksum = digits_sum + digits_not_multiplied_sum;
 
 
 
-    if((checksum % 10) == 0)
+    if ((checksum % 10) == 0)
     {
 
-        if(count == 15 && digit == 3)
+
+        if (count == 15 && digit == 3 && (seconddigit == 4 || seconddigit == 7))
             printf("AMEX\n");
-        else if(count == 16 && digit == 5)
+        else if (count == 16 && digit == 5 && seconddigit < 6)
             printf("MASTERCARD\n");
-        else if((count == 13 || count == 16) && digit == 4)
+        else if ((count == 13 || count == 16) && digit == 4)
             printf("VISA\n");
         else
             printf("INVALID\n");
